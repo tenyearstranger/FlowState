695bff82b742599d1e994d70f8998da116cb7399
yuki <21373039@buaa.edu.cn>
2026-05-04 14:14:24 +0800

    client-end init

---
+++ electron/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+
+const isDev = !app.isPackaged
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  })
+
+  if (isDev) {
+    win.loadURL('http://127.0.0.1:5173')
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
